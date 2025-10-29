3a1e3d3a83715c076f247975aa2d0e381ded5ea2
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-10-28 21:36:58 -0300

    commit

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -79,6 +79,7 @@
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img src="{{ asset('images/fotocapa2.jpg') }}" class="d-block w-100" alt="Capa 1">
+          
         </div>
         <div class="carousel-item">
           <!--<img src="{{ asset('images/fotocapa2.jpg') }}" class="d-block w-100" alt="Capa 2">-->
